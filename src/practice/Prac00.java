172ef32ccc8fb7f2f52eb52b8ede39914ec572bf
KwonIII <ig1108@nate.com>
2024-02-21 16:26:16 +0900

    study

---
+++ src/practice/Prac00.java
@@ -0,0 +1,11 @@
+package practice;
+
+//다음과 같이 출력하도록 코드를 작성하세요.
+//!@#$%^&*(\'"<>?:;
+public class Prac00 {
+
+	public static void main(String[] args) {
+		System.out.println("!@#$%^&*(\\\'\"<>?:;");
+	}
+
+}
---
+++ src/practice/Prac03.java
@@ -0,0 +1,24 @@
+package practice;
+
+import java.util.Scanner;
+
+//문자열 str이 주어집니다.
+//문자열을 시계방향으로 90도 돌려서 아래 입출력 예와 같이 출력하는 코드를 작성해 보세요.
+//abcde
+//a
+//b
+//c
+//d
+//e
+public class Prac03 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		
+		for(int i = 0;i<a.length();i++) {
+			System.out.println(a.charAt(i));
+		}
+	}
+
+}
